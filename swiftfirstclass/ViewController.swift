84d60d24496195f973deb2c5e8403b8f5aa7f456
Nisa CV <nisacv9@gmail.com>
2024-01-27 10:21:57 -0800

    Changes

--- swiftfirstclass/ViewController.swift
+++ swiftfirstclass/ViewController.swift
@@ -8,10 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtMsg: UILabel!
+    @IBOutlet weak var txtView: UITextView!
+    @IBOutlet weak var txtAge: UITextField!
+    @IBOutlet weak var txtCountry: UITextField!
+    @IBOutlet weak var txtLast: UITextField!
+    @IBOutlet weak var txtFirst: UITextField!
+    var x = 0;
+    
+    @IBAction func txtClear(_ sender: Any) {
+        txtFirst.text = ""
+        txtLast.text = ""
+        txtCountry.text = ""
+        txtAge.text = ""
+        txtView.text = ""
+        txtMsg.text = ""
+    }
+    @IBAction func txtSubmit(_ sender: Any) {
+        print("txtSubmit called")
+        if Validation_Check() == 1
+        {
+            txtMsg.text = "Complete the Missing Info!"
+            txtMsg.alpha = 1        }
+        else
+        {
+            print("enter in else")
+            txtMsg.text = "Succesfully Completed"
+            txtMsg.alpha = 1        }
+    }
+   
+    
+    
+    func Validation_Check() ->Int
+    {
+        print("validation check called")
+        if txtFirst.text == "" || txtLast.text == "" || txtCountry.text == "" || txtAge.text == ""
+        {
+            x = 1
+        }
+        else
+        {
+            x = 0
+        }
+        print(x)
+        return x
+            
+    }
+    @IBAction func addBtn(_ sender: Any) {
+       if Validation_Check() == 1
+        {
+           txtMsg.text = "Complete the Missing Info!";
+           txtMsg.alpha = 1
+        }
+        else
+        {
+            txtView.text = "First Name :\(txtFirst.text!)\(txtLast.text!)\n Country:\(txtCountry.text!)\n Age : \(txtAge.text!)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        //txtSubmit.isHidden = true
+        // txtFirst.text = "Nisa"
+      //  print(txtFirst.text);
+        
     }
 
 
